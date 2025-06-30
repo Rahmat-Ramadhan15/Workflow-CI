cbc546d039ea122255fa2347c553d7cbee5b8818
Rahmat Ramadhan <rahmatrmdhnrr15@gmail.com>
2025-06-30 16:23:17 +0800

    Update modelling.py

--- modelling.py
+++ modelling.py
@@ -7,40 +7,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import sys
 
-# Argument parsing dari MLproject entry_point
+# ======= Argument parsing dari MLproject entry_point ========
 n_estimators_default = int(sys.argv[1]) if len(sys.argv) > 1 else 200
 max_depth_default = int(sys.argv[2]) if len(sys.argv) > 2 else 10
 dataset_path = sys.argv[3] if len(sys.argv) > 3 else "data_automate_processing.csv"
+# ============================================================
 
 # Set nama eksperimen
 mlflow.set_experiment("Telco-Customer-Churn-Tuning")
 
-# Start run sebagai nested
-with mlflow.start_run(run_name=f"Tuning_RF_n{n_estimators_default}_d{max_depth_default}", nested=True):
-    mlflow.sklearn.autolog()
+# Aktifkan autologging
+mlflow.sklearn.autolog()
 
-    # Load dataset
-    df = pd.read_csv(dataset_path)
-    X = df.drop("Churn", axis=1)
-    y = df["Churn"]
+# Load dataset
+df = pd.read_csv(dataset_path)
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
 
-    # Split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Train model
-    model = RandomForestClassifier(
-        n_estimators=n_estimators_default,
-        max_depth=max_depth_default,
-        random_state=42
-    )
-    model.fit(X_train, y_train)
+# Train model
+model = RandomForestClassifier(
+    n_estimators=n_estimators_default,
+    max_depth=max_depth_default,
+    random_state=42
+)
+model.fit(X_train, y_train)
 
-    # Log akurasi
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    mlflow.log_metric("manual_accuracy", acc)
+# Evaluate & log metric
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+mlflow.log_metric("manual_accuracy", acc)
 
-    # Log model
-    mlflow.sklearn.log_model(model, artifact_path="model")
+# Simpan model ke artifact
+mlflow.sklearn.log_model(model, artifact_path="model")
 
-    print(f"✅ Model selesai dengan akurasi: {acc:.4f}")
+print(f"✅ Model selesai dengan akurasi: {acc:.4f}")
